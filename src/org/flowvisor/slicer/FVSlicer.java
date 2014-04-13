1149e18ad5655c101912489dd5a72f170a34facc
unknown <murad.kaplan@colorado.edu>
2014-04-12 22:50:50 -0600

    .

--- src/org/flowvisor/slicer/FVSlicer.java
+++ src/org/flowvisor/slicer/FVSlicer.java
@@ -571,6 +571,7 @@ public class FVSlicer implements FVEventHandler, FVSendMsg, FlowvisorChangedList
 			if (msgs == null)
 				throw new IOException("got null from read()");
 			for (OFMessage msg : msgs) {
+				System.out.println("MURAD: " + msg);
 				FVLog.log(LogLevel.INFO, this, "MURAD: recv from controller: ", msg);
 				this.stats.increment(FVStatsType.SEND, this, msg);
 				if ((msg instanceof SanityCheckable)
