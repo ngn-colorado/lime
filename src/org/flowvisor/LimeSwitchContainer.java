243f7cb3c2c25f21731c5a0c2ba0f82c4489651b
unknown <murad.kaplan@colorado.edu>
2014-04-18 16:53:12 -0600

    .

--- src/org/flowvisor/LimeSwitchContainer.java
+++ src/org/flowvisor/LimeSwitchContainer.java
@@ -17,12 +17,32 @@ public class LimeSwitchContainer {
 	private static HashMap<String, FVFeaturesReply> allNetworkSwitches = new HashMap<>();
 	
 	// list of only showing switches to OF controller
-	private static Set<String> showingSwitches = new HashSet<>();
+	private static Set<String> seenSwitches = new HashSet<>();
 	
 	// list of all classifiers created
-	private static HashMap<String, FVFeaturesReply> allNetworkClassifiers = new HashMap<>();
+	private static HashMap<String, FVFeaturesReply> allClassifiers = new HashMap<>();
 	
 	// list of all slicers created
-	private static HashMap<String, FVSlicer> allNetworkSlicers = new HashMap<>();
+	private static HashMap<String, FVSlicer> allSlicers = new HashMap<>();
+	
+	
+	
+	public static HashMap<String, FVFeaturesReply> getAllNetworkSwitcher(){
+		return allNetworkSwitches;
+	}
+	
+	public static Set<String> getSeenSwitches(){
+		return seenSwitches;
+	}
+	
+	public static HashMap<String, FVFeaturesReply> getAllClassifiers(){
+		return allClassifiers;
+	}
+	
+	public static HashMap<String, FVSlicer> getAllSlicers(){
+		return allSlicers;
+		
+	}
+	
 	
 }
