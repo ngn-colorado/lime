26029f14c65ce3f1fac590e4f7f6de43d4b15308
unknown <murad.kaplan@colorado.edu>
2014-05-08 14:52:59 -0600

    update all

--- src/org/flowvisor/message/FVPacketOut.java
+++ src/org/flowvisor/message/FVPacketOut.java
@@ -133,7 +133,10 @@ public class FVPacketOut extends OFPacketOut implements Classifiable, Slicable {
 		short originalPort = -1;
 		for (OFAction action : this.getActions()){
 			if(action instanceof OFActionOutput){
-				System.out.println("MURAD: FVPacketOut, output action port: " + ((OFActionOutput) action).getPort());
+				if(fvClassifier.getActivePorts() == null){
+					System.out.println("MURAD: FVPacketOut, activePorts is null");
+				}
+				
 				if(fvClassifier.getActivePorts().containsKey(((OFActionOutput) action).getPort())){
 					if (fvClassifier.getActivePorts().get(((OFActionOutput) action).getPort()).getType().equals(PortType.EMPTY)){ 
 						originalPort = ((OFActionOutput) action).getPort();
