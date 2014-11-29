8cca7675577570a48498a69b2b50a7af1b609aa0
Michael Coughlin <michael.coughlin@colorado.edu>
2014-11-29 03:38:06 -0700

    put handling server connections into thread

--- src/org/flowvisor/api/LimeServer.java
+++ src/org/flowvisor/api/LimeServer.java
@@ -20,51 +20,67 @@ import org.flowvisor.LimeContainer;
 import org.flowvisor.LimeMigrationHandler;
 import org.flowvisor.classifier.WorkerSwitch;
 
-public  class LimeServer implements Runnable{
-		
+public class LimeServer implements Runnable {
+
 	@Override
-	public void run(){
+	public void run() {
 		System.out.println("MURAD: LimeServer is running...");
 		int portNumber = 8082;
 
-		try (
-				ServerSocket serverSocket =
-				new ServerSocket(portNumber);
-				Socket clientSocket = serverSocket.accept();
-				PrintWriter out =
-						new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(
-						new InputStreamReader(clientSocket.getInputStream()));
-				) {
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				System.out.println("Recv from operator: " + inputLine);
-				out.println("LIME said: " + inputLine);
-				LimeMigrationHandler limeMigHandler = null;
-				if(inputLine.equals("start")){
-					limeMigHandler = new LimeMigrationHandler();
-					limeMigHandler.init();
-				} else if(inputLine.startsWith("migration finished: ")){
-					//message must be in form: "migration finished: original_dpid clone_dpid
-					if(limeMigHandler == null){
-						System.out.println("migration must be started first");
-						continue;
-					}
-					String[] tokens = inputLine.split(" ");
-					DPID originalDPID = new DPID(tokens[2]);
-					DPID cloneDPID = new DPID(tokens[3]);
-					WorkerSwitch originalSwitch = LimeContainer.getAllWorkingSwitches().get(originalDPID.getDpidLong());
-					WorkerSwitch cloneSwitch = LimeContainer.getAllWorkingSwitches().get(cloneDPID.getDpidLong());
-					limeMigHandler.switchDoneMigrating(cloneSwitch, originalSwitch);
-				}
+		try {
+			while(true){
 				
+				ServerSocket serverSocket = new ServerSocket(portNumber);
+				final Socket clientSocket = serverSocket.accept();
+				Runnable handlerTask = new Runnable(){
+					@Override
+					public void run() {
+						System.out.println("Starting new handler thread");
+						try {
+							PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+							BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+							
+							String inputLine;
+							while ((inputLine = in.readLine()) != null) {
+								System.out.println("Recv from operator: " + inputLine);
+								out.println("LIME said: " + inputLine);
+								LimeMigrationHandler limeMigHandler = null;
+								if (inputLine.equals("start")) {
+									limeMigHandler = new LimeMigrationHandler();
+									limeMigHandler.init();
+								} else if (inputLine.startsWith("migration finished: ")) {
+									// message must be in form: "migration finished:
+									// original_dpid clone_dpid
+									if (limeMigHandler == null) {
+										System.out.println("migration must be started first");
+										continue;
+									}
+									String[] tokens = inputLine.split(" ");
+									DPID originalDPID = new DPID(tokens[2]);
+									DPID cloneDPID = new DPID(tokens[3]);
+									WorkerSwitch originalSwitch = LimeContainer.getAllWorkingSwitches().get(originalDPID.getDpidLong());
+									WorkerSwitch cloneSwitch = LimeContainer.getAllWorkingSwitches().get(cloneDPID.getDpidLong());
+									limeMigHandler.switchDoneMigrating(cloneSwitch, originalSwitch);
+								}
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				};
+				
+				Thread handlerThread = new Thread(handlerTask);
+				handlerThread.start();
 			}
-		} catch (IOException | InterruptedException e) {
-			System.out.println("Exception caught when trying to listen on port "
+		} catch (IOException e) {
+			System.out
+			.println("Exception caught when trying to listen on port "
 					+ portNumber + " or listening for a connection");
 			System.out.println(e.getMessage());
 		}
 	}
 }
-
-
