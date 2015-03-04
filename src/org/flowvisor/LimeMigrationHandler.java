935cd0edaf4497567309e460ab09212a1c86d331
Michael Coughlin <michael.coughlin@colorado.edu>
2015-03-03 22:57:03 -0700

    fixed multiple outputs to ghost port and re-added vlan action

--- src/org/flowvisor/LimeMigrationHandler.java
+++ src/org/flowvisor/LimeMigrationHandler.java
@@ -659,8 +659,9 @@ public final class LimeMigrationHandler {
 				//add vlan tag action to mod
 				//insert vlan action before the output action in the action list
 				int actionIndex = clonedMod.getActions().indexOf(migratedAction);
-//				clonedMod.getActions().add(actionIndex, addedVlanAction);
+				clonedMod.getActions().add(actionIndex, addedVlanAction);
 				//only need to output out ghost port once
+				ghostPortRuleWritten = true;
 			}
 		}
 		
