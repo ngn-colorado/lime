39fd3fa644ccc3e551c2bbdbb399eca9fd7daec0
Michael Coughlin <michael.coughlin@colorado.edu>
2015-03-07 22:17:31 -0700

    added missed exception class

---
+++ src/edu/colorado/cs/ngn/lime/exceptions/LimeDummyPortNotFoundException.java
@@ -0,0 +1,30 @@
+package edu.colorado.cs.ngn.lime.exceptions;
+
+public class LimeDummyPortNotFoundException extends Exception {
+
+	public LimeDummyPortNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LimeDummyPortNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LimeDummyPortNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LimeDummyPortNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LimeDummyPortNotFoundException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
